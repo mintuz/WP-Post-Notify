2de5a869a9c824f28e1e26974df75c4d8697bad3
Adam Bulmer <mintuz1990@gmail.com>
2012-12-22 22:02:31 +0000

    version 0.2, now has the show_subscribe_widget method.

--- notify.php
+++ notify.php
@@ -5,7 +5,7 @@
 Plugin Name: WP Post Notify
 Plugin URI: http://www.mintuz.com
 Description: Notify subscribers of a new post
-Version: 0.1
+Version: 0.2
 Author: Adam Bulmer
 Author URI: http://www.mintuz.com
 
@@ -15,7 +15,7 @@ Author URI: http://www.mintuz.com
 // ! CONSTANTS
 // =============
 
-define( 'WPP_VERSION', '0.1' );
+define( 'WPP_VERSION', '0.2' );
 define( 'WPP_DIR', WP_PLUGIN_DIR . '/' . basename( dirname(__FILE__) ) );
 define( 'WPP_URL', rtrim( plugin_dir_url( __FILE__ ), '/' ) );
 define( 'WPP_CUSTOM_TABLE' , 'wpp_subscribers' );
@@ -28,8 +28,10 @@ define( 'WPP_CUSTOM_TABLE' , 'wpp_subscribers' );
 include WPP_DIR . '/classes/WPPBackendUI.class.inc';
 include WPP_DIR . '/classes/WPPConfig.class.inc';
 include WPP_DIR . '/classes/WPPTemplateManager.class.inc';
+include WPP_DIR . '/classes/WPPTemplateTags.class.inc';
 include WPP_DIR . '/classes/WPPNotificationCentre.class.inc';
 include WPP_DIR . '/classes/WPPUtils.class.inc';
+include WPP_DIR . '/classes/WPPFrontendUI.class.inc';
 
 // Third Party Classes
 include WPP_DIR . '/libs/php_extended.inc';
@@ -58,5 +60,15 @@ add_action( 'save_post', array( 'WPPConfig', 'save_post' ) );
 // Actions
 add_action( 'add_meta_boxes' , array( 'WPPBackendUI', 'add_meta_boxes' ) );
 
+// ==================
+// ! WPPBackendUI
+// ==================
+
+// Shortcodes
+
+// Filters
+
+// Actions
+add_action( 'wp_enqueue_scripts' , array( 'WPPFrontendUI', 'wp_enqueue_scripts' ) );
 
 ?>
